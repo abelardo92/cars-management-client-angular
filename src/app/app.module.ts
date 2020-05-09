a948a44502e11a54791a5e7e5fe017088abaf4ec
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-09 16:10:44 -0500

    service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { routing, appRoutingProviders } from './app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +19,8 @@ import { RegisterComponent } from './components/register/register.component';
     BrowserModule,
     routing,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     appRoutingProviders
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
+  providers: [UserService],
   styleUrls: ['./register.component.sass']
 })
 export class RegisterComponent implements OnInit {
   public title:string;
   public user:User;
 
-  constructor() {
+  constructor(
+    private _route: ActivatedRoute,
+    private_router: Router,
+    private _userService: UserService
+  ) {
     this.title = "Register";
     this.user = new User(1, 'ROLE_USER','','','','');
   }
 
   onSubmit() {
-    console.log('register.component loaded succesfully');
     console.log(this.user);
+    console.log(this._userService.test());
   }
 
   ngOnInit(): void {
