aad8598bdd33c479bb7ca1cd6110281e65a993c1
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-09 19:55:00 -0500

    login method implemented

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
+  providers: [UserService],
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
@@ -14,7 +16,8 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private _route: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _userService: UserService
   ) {
     this.title = "Set your identity";
     this.user = new User(1, 'ROLE_USER','','','','');
@@ -25,7 +28,33 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(form) {
-    console.log(this.user);
+
+    this._userService.signup(this.user).subscribe(
+      response => {
+        // token
+        console.log(response);
+
+        this._userService.signup(this.user, true).subscribe(
+          response => {
+            console.log(response);
+          },
+          error => {
+            console.log(<any>error);
+          });
+
+
+        // this.status = response.status;
+        // if(this.status == 'success') {
+        //   this.user = new User(1, 'ROLE_USER','','','','');
+        //   form.reset();
+        // } else {
+        //   this.status = 'error';
+        // }
+      },
+      error => {
+        this.status = 'error';
+      }
+    )
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,22 +2,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { GLOBAL } from './global';
+import { User } from '../models/user';
 
 @Injectable()
 export class UserService {
   public url: string;
+  public headers: HttpHeaders;
 
   constructor(
     public _http: HttpClient
   ) {
     this.url = GLOBAL.url;
+    this.headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded');
   }
 
   register(user): Observable<any> {
     let json = JSON.stringify(user);
     let params = 'json='+json;
-    let headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded');
-    return this._http.post(this.url+'register', params, { headers: headers });
+    return this._http.post(this.url+'register', params, { headers: this.headers });
+  }
+
+  signup(user, getToken = null): Observable<any> {
+    if(getToken) {
+      user.get_token = true;
+    }
+    let json = JSON.stringify(user);
+    let params = 'json='+json;
+    console.log(json);
+    return this._http.post(this.url+'login', params, { headers: this.headers });
   }
 
   test() {
