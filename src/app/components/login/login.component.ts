9c7aea2799a7c6ce23dd6d7abe01f86dcbe554f4
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-09 19:27:01 -0500

    login form added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'login',
@@ -8,16 +9,23 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   public title: string;
+  public user:User;
+  public status: string;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router
   ) {
     this.title = "Set your identity";
+    this.user = new User(1, 'ROLE_USER','','','','');
   }
 
   ngOnInit(): void {
     console.log("login loaded succesfully!!")
+  }
+
+  onSubmit(form) {
+    console.log(this.user);
   }
 
 }
