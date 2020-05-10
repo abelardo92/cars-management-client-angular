dfdcae2e54eb33938ffacc78ae5fbce5f5af0aff
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-10 14:12:24 -0500

    new components have been generated

---
+++ src/app/components/car-edit/car-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-edit',
+  templateUrl: './car-edit.component.html',
+  styleUrls: ['./car-edit.component.sass']
+})
+export class CarEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/car-new/car-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-new',
+  templateUrl: './car-new.component.html',
+  styleUrls: ['./car-new.component.sass']
+})
+export class CarNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
