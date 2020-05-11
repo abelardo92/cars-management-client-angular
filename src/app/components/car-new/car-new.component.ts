1726fc799a4ca0bf01375b013322ab2d0ccfdfca
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-10 20:55:19 -0500

    validation in case user is not logged in has been implemented

--- src/app/components/car-new/car-new.component.ts
+++ src/app/components/car-new/car-new.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { Car } from 'src/app/models/car';
 
 @Component({
   selector: 'app-car-new',
   templateUrl: './car-new.component.html',
+  providers: [UserService],
   styleUrls: ['./car-new.component.sass']
 })
 export class CarNewComponent implements OnInit {
+  public title: string;
+  public identity;
+  public token;
+  public car: Car;
+  public status: string;
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService
+  ) {
+    this.title = "Add new car";
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+   }
 
   ngOnInit(): void {
+    if(this.identity == null) {
+      this._router.navigate(['/login']);
+    } else {
+      this.car = new Car(1, '', '', 1, '', null, null);
+    }
   }
 
 }
