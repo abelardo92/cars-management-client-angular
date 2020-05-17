a27a5902ad89322919ff3c4e37268e49e09a0c73
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-17 12:31:11 -0500

    car can now be created

--- src/app/components/car-new/car-new.component.ts
+++ src/app/components/car-new/car-new.component.ts
@@ -1,37 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import { Car } from 'src/app/models/car';
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'app-car-new',
   templateUrl: './car-new.component.html',
-  providers: [UserService],
+  providers: [UserService, CarService],
   styleUrls: ['./car-new.component.sass']
 })
-export class CarNewComponent implements OnInit {
-  public title: string;
+export class CarNewComponent implements OnInit, DoCheck {
+  public title_form: string;
   public identity;
   public token;
   public car: Car;
-  public status: string;
+  public status_result: string;
+  public error: string;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _userService: UserService
+    private _userService: UserService,
+    private _carService: CarService
   ) {
-    this.title = "Add new car";
+    this.title_form = "Add new car";
+    console.log(this.title_form);
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
-   }
+    // console.log(this.status);
+  }
 
   ngOnInit(): void {
     if(this.identity == null) {
       this._router.navigate(['/login']);
     } else {
       this.car = new Car(1, '', '', 1, '', null, null);
+      this.status_result = '';
+      this.error = '';
+      console.log("status: " + this.status_result);
     }
+  }
+
+  onSubmit(form) {
+    // console.log(this.car);
+    this._carService.create(this.token, this.car).subscribe(
+      response => {
+        console.log(response.error);
+        if(response.status != 'success') {
+          this.error = response.error;
+          this.status_result = 'error';
+        } else {
+          this.status_result = 'success'
+          this.car = response.car;
+          this._router.navigate(['/home']);
+        }
+        console.log(this.status_result);
+
+      },
+      error => {
+        this.status_result = 'error';
+        // this.error = error;
+      }
+    );
+  }
+
+  ngDoCheck(): void {
+    // this.refreshData();
   }
 
 }
---
+++ src/app/services/car.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { GLOBAL } from './global';
+import { Car } from '../models/car';
+
+@Injectable()
+export class CarService {
+  public url: string;
+  public headers: HttpHeaders;
+  public identity;
+  public token;
+
+  constructor(
+    public _http: HttpClient
+  ) {
+    this.url = GLOBAL.url;
+  }
+
+  create(token, car: Car) : Observable<any> {
+    let json = JSON.stringify(car);
+    let params = 'json='+json;
+    this.headers = new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded').set('Authorization', token);
+    return this._http.post(this.url+'cars', params, { headers: this.headers });
+  }
+
+}
